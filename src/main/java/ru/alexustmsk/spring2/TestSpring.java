8a7e36158a777267ae96c5d84b6f49221c3d9e87
Alexander <alexustmsk@gmail.com>
2020-01-08 19:52:13 +0300

    Изучаю Spring Framework

---
+++ src/main/java/ru/alexustmsk/spring2/TestSpring.java
@@ -0,0 +1,17 @@
+package ru.alexustmsk.spring2;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContex.xml"
+        );
+
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+
+        System.out.println(testBean.getName());
+
+        context.close();
+    }
+}
